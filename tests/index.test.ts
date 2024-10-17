277ab95661b8ad002fa04eda619c289bd7301beb
Felipe <felipe.chawischi@catolicasc.edu.br>
2024-10-16 21:55:04 -0300

    Validate scope

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,3 +5,10 @@ describe('numeroPorExtenso', () => {
         expect(ext(0)).toBe('zero');
     });
 });
+
+describe('numeroPorExtenso', () => {
+    test('deve retornar o número null se fora do escopo', () => {
+        expect(ext(10)).toBe("");
+        expect(ext(-1)).toBe("");
+    });
+});
